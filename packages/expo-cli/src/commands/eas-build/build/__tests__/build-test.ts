be66ebdfa9247990b18cde8a5d447d4b18e566ba
Cedric van Putten <me@bycedric.com>
2020-08-10 17:02:10 +0200

    [cli] Update project mocks to include getProjectData

--- packages/expo-cli/src/commands/eas-build/build/__tests__/build-test.ts
+++ packages/expo-cli/src/commands/eas-build/build/__tests__/build-test.ts
@@ -34,7 +34,9 @@ jest.mock('@expo/config', () => {
 jest.mock('fs');
 jest.mock('prompts');
 jest.mock('../../../../projects', () => {
+  const { getProjectData } = jest.requireActual('../../../../projects');
   return {
+    getProjectData,
     ensureProjectExistsAsync: () => 'fakeProjectId',
   };
 });
